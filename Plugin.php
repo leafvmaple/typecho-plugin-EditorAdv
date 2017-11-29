6caa302558e4bcf829f7b131a3592ca93d6f694e
LeafMaple <leafvmaple@163.com>
2017-11-29 09:16:20 +0900

    Support pjaxComplete

--- Plugin.php
+++ Plugin.php
@@ -13,7 +13,7 @@ class EditorAdv_Plugin implements Typecho_Plugin_Interface
      * 插件版本号
      * @var string
      */
-    const _VERSION = '0.2';
+    const _VERSION = '0.3';
     /**
      * 激活插件方法,如果激活失败,直接抛出异常
      *
@@ -27,6 +27,7 @@ class EditorAdv_Plugin implements Typecho_Plugin_Interface
         Typecho_Plugin::factory('admin/write-page.php')->bottom = array('EditorAdv_Plugin', 'button');
         Typecho_Plugin::factory('Widget_Archive')->header = array('EditorAdv_Plugin', 'header');
         Typecho_Plugin::factory('Widget_Archive')->footer = array('EditorAdv_Plugin', 'footer');
+        Typecho_Plugin::factory('component/footer.php')->pjaxComplete = array('EditorAdv_Plugin', 'pjaxComplete');
     }
     
 
@@ -47,6 +48,9 @@ class EditorAdv_Plugin implements Typecho_Plugin_Interface
         'Midnight' => 'Midnight',
         'RDark' => 'RDark'), 'Default', _t('高亮主题:'), _t('选择一个你喜欢的高亮主题。'));
         $form->addInput($theme);
+
+        $codeButton = new Typecho_Widget_Helper_Form_Element_Checkbox('codeButton', array('codeButton' => '显示按钮'), NULL, _t('显示按钮'), _t('是否显示文章编辑栏上的代码按钮'));
+        $form->addInput($codeButton);
 
         $collapse = new Typecho_Widget_Helper_Form_Element_Checkbox('collapse', array('collapse' => '折叠代码'), NULL, _t('代码折叠'), _t('是否自动折叠代码，点击时展开（开启时，请同时开启显示工具栏，不然代码无法显示）'));
         $form->addInput($collapse);
@@ -94,60 +98,60 @@ class EditorAdv_Plugin implements Typecho_Plugin_Interface
      * @return unknown
      */
     public static function button() {
-        ?><style>.wmd-button-row {
-    height: auto;
-}</style>
-        <script> 
-            $(document).ready(function(){
-                $('#wmd-button-row').append('<li class="wmd-button" id="wmd-jrotty-button" title="代码 - ALT+C"><span style="background: none;font-size: large;text-align: center;color: #999999;font-family: serif;">C</span></li>');
-                if($('#wmd-button-row').length !== 0){
-                    $('#wmd-jrotty-button').click(function(){
-                        var rs = "```\ncode\n```\n";
-                        tagfunc(rs);
-                    })
-                }
+        $settings = Helper::options()->plugin('EditorAdv');
 
+        $codeButton = 'false';
+        if ($settings->codeButton && in_array('codeButton', $settings->codeButton))
+            $codeButton = 'true';
+        
+        echo <<<EOF
+        <style>.wmd-button-row {
+            height: auto;
+        }</style>
+        <script>
+            if ($codeButton) {
+                $(document).ready(function() {
+                    $('#wmd-button-row').append('<li class="wmd-button" id="wmd-jrotty-button" title="代码 - ALT+C"><span style="background: none;font-size: large;text-align: center;color: #999999;font-family: serif;">C</span></li>');
+                    if($('#wmd-button-row').length !== 0) {
+                        $('#wmd-jrotty-button').click(function() {
+                            var str = "```\\ncode\\n```\\n";
+                            insertStr(str);
+                        })
+                    }
 
-                function tagfunc(tag) {
-                    var myField;
-                    if (document.getElementById('text') && document.getElementById('text').type == 'textarea') {
-                        myField = document.getElementById('text');
-                    } else {
-                        return false;
+                    function insertStr(str) {
+                        var text;
+                        if (document.getElementById('text') && document.getElementById('text').type == 'textarea') {
+                            text = document.getElementById('text');
+                        } else {
+                            return false;
+                        }
+                        if (document.selection) {
+                            sel = document.selection.createRange();
+                            sel.text = str;
+                        }
+                        else if (text.selectionStart || text.selectionStart == '0') {
+                            var curPos = text.selectionStart;
+                            text.value = text.value.substring(0, text.selectionStart) + str + text.value.substring(text.selectionEnd, text.value.length);
+                            curPos += str.length;
+                            text.selectionStart = curPos;
+                            text.selectionEnd = curPos;
+                        } else {
+                            text.value += str;
+                        }
+                        text.focus();
                     }
-                    if (document.selection) {
-                        myField.focus();
-                        sel = document.selection.createRange();
-                        sel.text = tag;
-                        myField.focus();
-                    }
-                    else if (myField.selectionStart || myField.selectionStart == '0') {
-                        var startPos = myField.selectionStart;
-                        var endPos = myField.selectionEnd;
-                        var cursorPos = startPos;
-                        myField.value = myField.value.substring(0, startPos)
-                        + tag
-                        + myField.value.substring(endPos, myField.value.length);
-                        cursorPos += tag.length;
-                        myField.focus();
-                        myField.selectionStart = cursorPos;
-                        myField.selectionEnd = cursorPos;
-                    } else {
-                        myField.value += tag;
-                        myField.focus();
-                    }
-                }
 
-                $('body').on('keydown',function(a){
-                    if( a.altKey && a.keyCode == "67"){
-                        $('#wmd-jrotty-button').click();
-                    }
+                    $('body').on('keydown',function(a) {
+                        if( a.altKey && a.keyCode == "67") {
+                            $('#wmd-jrotty-button').click();
+                        }
+                    });
                 });
-
-
-            });
-</script>
-<?php
+            }
+        </script>
+EOF;
+        echo "\n";
     }
 
     /**
@@ -167,15 +171,15 @@ class EditorAdv_Plugin implements Typecho_Plugin_Interface
     }
 
     /**
-     * 输出尾部js
+     * 代码高亮
      *
      * @access public
-     * @param unknown $footer
+     * @param bool $ScriptSign
      * @return unknown
      */
-    public static function footer() {
+    static function highlighter($ScriptSign) {
         $settings = Helper::options()->plugin('EditorAdv');
-
+        
         $collapse = 'false';
         if ($settings->collapse && in_array('collapse', $settings->collapse))
             $collapse = 'true';
@@ -200,64 +204,90 @@ class EditorAdv_Plugin implements Typecho_Plugin_Interface
 
         $currentPath = Helper::options()->pluginUrl . '/EditorAdv/';
 
+        if ($ScriptSign) {
+            echo '<script>';
+        }
+
         echo <<<EOF
-        <script type="text/javascript">
-            if (typeof(EditorAdv) !== undefined) {
-                var preList = document.getElementsByTagName('pre');
-                for (var i = 0; i < preList.length; i ++) {
-                    var children = preList[i].getElementsByTagName('code');
-                    if (children.length > 0) {
-                        var language = 'plain';
-                        var code = children[0], className = code.className;
-                        if (!!className) {
-                            var match = XRegExp.exec(className, XRegExp('^(lang|language)-(?<language>.*)$'));
-                            if (match && match.language) {
-                                language = match.language;
-                            }
+        if (typeof(SyntaxHighlighter) != undefined) {
+            var preList = document.getElementsByTagName('pre');
+            for (var i = 0; i < preList.length; i ++) {
+                var children = preList[i].getElementsByTagName('code');
+                if (children.length > 0) {
+                    var language = 'plain';
+                    var code = children[0], className = code.className;
+                    if (!!className) {
+                        var match = XRegExp.exec(className, XRegExp('^(lang|language)-(?<language>.*)$'));
+                        if (match && match.language) {
+                            language = match.language;
                         }
-                        preList[i].className = 'brush: ' + language;
-                        preList[i].innerHTML = code.innerHTML;
                     }
+                    preList[i].className = 'brush: ' + language;
+                    preList[i].innerHTML = code.innerHTML;
                 }
-                EditorAdv.autoloader(
-                        'applescript           {$currentPath}scripts/shBrushAppleScript.js',
-                        'ahk autohotkey        {$currentPath}scripts/shBrushAhk.js',
-                        'actionscript3 as3     {$currentPath}scripts/shBrushAS3.js',
-                        'bash shell            {$currentPath}scripts/shBrushBash.js',
-                        'bat cmd batch         {$currentPath}scripts/shBrushBat.js',
-                        'coldfusion cf         {$currentPath}scripts/shBrushColdFusion.js',
-                        'cpp c                 {$currentPath}scripts/shBrushCpp.js',
-                        'c# c-sharp csharp     {$currentPath}scripts/shBrushCSharp.js',
-                        'css                   {$currentPath}scripts/shBrushCss.js',
-                        'delphi pascal pas     {$currentPath}scripts/shBrushDelphi.js',
-                        'diff patch            {$currentPath}scripts/shBrushDiff.js',
-                        'erl erlang            {$currentPath}scripts/shBrushErlang.js',
-                        'groovy                {$currentPath}scripts/shBrushGroovy.js',
-                        'java                  {$currentPath}scripts/shBrushJava.js',
-                        'jfx javafx            {$currentPath}scripts/shBrushJavaFX.js',
-                        'js jscript javascript {$currentPath}scripts/shBrushJScript.js',
-                        'perl pl               {$currentPath}scripts/shBrushPerl.js',
-                        'php                   {$currentPath}scripts/shBrushPhp.js',
-                        'text plain            {$currentPath}scripts/shBrushPlain.js',
-                        'powershell ps         {$currentPath}scripts/shBrushPowerShell.js',
-                        'py python             {$currentPath}scripts/shBrushPython.js',
-                        'ruby rails ror rb     {$currentPath}scripts/shBrushRuby.js',
-                        'sass scss             {$currentPath}scripts/shBrushSass.js',
-                        'scala                 {$currentPath}scripts/shBrushScala.js',
-                        'sql                   {$currentPath}scripts/shBrushSql.js',
-                        'vb vbnet              {$currentPath}scripts/shBrushVb.js',
-                        'xml xhtml xslt html   {$currentPath}scripts/shBrushXml.js'
-                        );
-                EditorAdv.defaults['auto-links'] = $autoLinks;
-                EditorAdv.defaults['collapse'] = $collapse;
-                EditorAdv.defaults['gutter'] = $gutter;
-                EditorAdv.defaults['smart-tabs'] = $smartTabs;
-                EditorAdv.defaults['tab-size'] = $tabSize;
-                EditorAdv.defaults['toolbar'] = $toolbar;
-                EditorAdv.all();
             }
-        </script>
+            SyntaxHighlighter.autoloader(
+                'applescript           {$currentPath}scripts/shBrushAppleScript.js',
+                'ahk autohotkey        {$currentPath}scripts/shBrushAhk.js',
+                'actionscript3 as3     {$currentPath}scripts/shBrushAS3.js',
+                'bash shell            {$currentPath}scripts/shBrushBash.js',
+                'bat cmd batch         {$currentPath}scripts/shBrushBat.js',
+                'coldfusion cf         {$currentPath}scripts/shBrushColdFusion.js',
+                'cpp c                 {$currentPath}scripts/shBrushCpp.js',
+                'c# c-sharp csharp     {$currentPath}scripts/shBrushCSharp.js',
+                'css                   {$currentPath}scripts/shBrushCss.js',
+                'delphi pascal pas     {$currentPath}scripts/shBrushDelphi.js',
+                'diff patch            {$currentPath}scripts/shBrushDiff.js',
+                'erl erlang            {$currentPath}scripts/shBrushErlang.js',
+                'groovy                {$currentPath}scripts/shBrushGroovy.js',
+                'java                  {$currentPath}scripts/shBrushJava.js',
+                'jfx javafx            {$currentPath}scripts/shBrushJavaFX.js',
+                'js jscript javascript {$currentPath}scripts/shBrushJScript.js',
+                'perl pl               {$currentPath}scripts/shBrushPerl.js',
+                'php                   {$currentPath}scripts/shBrushPhp.js',
+                'text plain            {$currentPath}scripts/shBrushPlain.js',
+                'powershell ps         {$currentPath}scripts/shBrushPowerShell.js',
+                'py python             {$currentPath}scripts/shBrushPython.js',
+                'ruby rails ror rb     {$currentPath}scripts/shBrushRuby.js',
+                'sass scss             {$currentPath}scripts/shBrushSass.js',
+                'scala                 {$currentPath}scripts/shBrushScala.js',
+                'sql                   {$currentPath}scripts/shBrushSql.js',
+                'vb vbnet              {$currentPath}scripts/shBrushVb.js',
+                'xml xhtml xslt html   {$currentPath}scripts/shBrushXml.js'
+            );
+            SyntaxHighlighter.defaults['auto-links'] = $autoLinks;
+            SyntaxHighlighter.defaults['collapse'] = $collapse;
+            SyntaxHighlighter.defaults['gutter'] = $gutter;
+            SyntaxHighlighter.defaults['smart-tabs'] = $smartTabs;
+            SyntaxHighlighter.defaults['tab-size'] = $tabSize;
+            SyntaxHighlighter.defaults['toolbar'] = $toolbar;
+            SyntaxHighlighter.all();
+        }
 EOF;
-        echo "\n";
+        if ($ScriptSign) {
+            echo '</script>\n';
+        }
+    }
+
+    /**
+     * 输出尾部js
+     *
+     * @access public
+     * @param unknown $footer
+     * @return unknown
+     */
+    public static function footer() {
+        EditorAdv_Plugin::highlighter(true);
+    }
+
+    /**
+     * 输出PjaxComplete
+     *
+     * @access public
+     * @param unknown $footer
+     * @return unknown
+     */
+    public static function pjaxComplete() {
+        EditorAdv_Plugin::highlighter(false);
     }
 }
